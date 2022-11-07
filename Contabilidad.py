bac607d0511e74ccf8ffdced56416f9881b8b6de
LuceroPerez1 <117342838+LuceroPerez1@users.noreply.github.com>
2022-11-06 20:27:38 -0600

    Add files via upload

---
+++ Contabilidad.py
@@ -0,0 +1,38 @@
+print("BIENVENIDO AL CÁLCULO DE PRESUPESTO")
+
+#LOS SIGUIENTES DATOS DEL BALANCE GENERAL
+
+print("Favor de ingresar las siguientes cantidades.")
+
+print("BALANCE GENERAL (AQUI VA LA FECHA)")
+print("ACTIVOS")
+efectivo = int(input("Efectivo: "))
+clientes = int(input("Clientes: "))
+deudores_diversos = int(input("Deudores diversos: "))
+inventario_materiales = int(input("Inventario de materiales: "))
+funcionarios_y_empleados = int(input("Funcionarios y empleados: "))
+inventario_prod_terminado = int(input("Inventario de producto terminado: "))
+#total_act_circulante = aqui va la suma de todo lo anterior
+terreno = int(input("Terreno: "))
+planta_equipo = int(input("Planta y equipo: "))
+dep_acumulada = int(input("Depreciacion acumulada: "))
+#total_act_no_circu = aqui va la suma de todo lo anterior
+#activo_total = aqui va la sumam de las dos sumas
+
+print("PASIVOS")
+proveedores = int(input("Proveedores: "))
+documentos_x_pagar = int(input("Documentos por pagar: "))
+isr_x_pagar = int(input("ISR por pagar: "))
+#total_pasiv_cort_plazo = suma de lo anterior
+prestamo_bancario = int(input("Prestamos bancarios: "))
+#total_pasiv_largo_plazo = aqui va la sumam de lo anterior
+#pasivo_total = suma de las dos sumas
+
+print("CAPITAL CONTABLE")
+capital_contribuido = int(input("Capital contribuido: "))
+capital_ganado = int(input("Capital ganado: "))
+#capital_contable_total = suma de lo anterior
+#suma_pasiv_cap = suma de pasivo y capital
+
+
+
